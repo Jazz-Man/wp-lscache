3e33ab5cbd3c5234a6ce9be5e82884e90041efb2
Vasyl Sokolyk <vsokolyk@gmail.com>
2021-06-06 13:59:17 +0300

    init repo

---
+++ wp-lscache.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: WP LiteSpeed Cache
+Description: WP LiteSpeed Cache
+Version: v1.0.0
+Plugin URI: https://github.com/Jazz-Man/wp-lscache
+Author: Vasyl Sokolyk
+Text Domain: wp-lscache
+Domain Path: /languages
+*/
